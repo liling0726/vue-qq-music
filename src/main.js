368870bf3be255510bb54d4798eb8f8ba480e79d
liling0726 <1329301856@qq.com>
2018-07-15 15:17:32 +0800

    添加公共函数

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
 
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
